620ec62fb76eb9fe8f62a73f0de151db5d292268
AlyshaPrivyet <445663962@qq.com>
2016-12-14 12:22:52 +0800

    首次上传工程

---
+++ RealReal/RealReal/RealReal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RealReal
+//
+//  Created by alysha on 2016/12/14.
+//  Copyright © 2016年 alysha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
